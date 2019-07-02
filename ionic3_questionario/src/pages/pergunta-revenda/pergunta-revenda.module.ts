0ee8c9b79c49d3e0ccb17c1a592ac9c03d97024f
Paulo Alexandre <paulofore@gmail.com>
2019-07-01 21:02:24 -0300

    primeiro commit

---
+++ ionic3_questionario/src/pages/pergunta-revenda/pergunta-revenda.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { PerguntaRevendaPage } from './pergunta-revenda';
+
+@NgModule({
+  declarations: [
+    PerguntaRevendaPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(PerguntaRevendaPage),
+  ],
+})
+export class PerguntaRevendaPageModule {}
