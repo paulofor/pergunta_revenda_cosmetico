2069d40e7ff0f421f28263c41a8be255830d388d
Paulo Alexandre <paulofore@gmail.com>
2019-07-04 21:29:22 -0300

    evoluindo quest

--- ionic3_questionario/src/pages/lista-marcas/lista-marcas.ts
+++ ionic3_questionario/src/pages/lista-marcas/lista-marcas.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { RespostaVersao, RespostaVersaoApi } from '../../app/shared/sdk/index';
+import { ListaLojasPage } from '../lista-lojas/lista-lojas';
 
 /**
  * Generated class for the ListaMarcasPage page.
@@ -17,11 +18,11 @@ import { RespostaVersao, RespostaVersaoApi } from '../../app/shared/sdk/index';
 export class ListaMarcasPage {
 
   respostaMarcas: RespostaVersao = new RespostaVersao();
-  marca1:String;
-  marca2:String;
-  marca3:String;
-  marca4:String;
-  marca5:String;
+  marca1: String;
+  marca2: String;
+  marca3: String;
+  marca4: String;
+  marca5: String;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private srv: RespostaVersaoApi) {
   }
@@ -31,10 +32,17 @@ export class ListaMarcasPage {
     this.respostaMarcas.pergunta = 'Suas marcas preferidas';
   }
 
-  enviaResposta(){
-    
-  }
 
+  submit() {
+    console.log('Chamou submit');
+    //this.respostaMarcas.resposta = this.marca1 + ' , ' + this.marca2 + ' , ' + this.marca3 + ' , ' + this.marca4 + ' , ' + this.marca5;
+    this.srv.create(this.respostaMarcas)
+      .subscribe((resultado) => {
+        console.log('Resultado Marcas: ', resultado);
+      });
+    this.navCtrl.push(ListaLojasPage, {
+    })
+  }
 
 
 }
