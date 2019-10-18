8164abc320136043d896e68379b033bd55b40dcd
tr626987 <tr626987@oi.corp.net>
2019-10-18 15:55:26 -0300

    visitas

--- ionic3_back/src/app/app.component.ts
+++ ionic3_back/src/app/app.component.ts
@@ -6,8 +6,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { LoginPage } from '../pages/login/login';
-import { AcessaFcmService } from '../servico/acessa-fcm-service';
-import { VERSAO_APP_ID } from './const';
 import { ListaOportunidadePage } from '../pages/lista-oportunidade/lista-oportunidade';
 import { ListaOportunidadePrecoPage } from '../pages/lista-oportunidade-preco/lista-oportunidade-preco';
 import { timer } from 'rxjs/observable/timer';
@@ -23,8 +21,7 @@ export class MyApp {
   pages: Array<{title: string, component: any}>;
   showSplash = true;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen,
-  				private fcmSrv:AcessaFcmService ) {
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
@@ -38,7 +35,6 @@ export class MyApp {
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      this.fcmSrv.executaValidacaoFake(VERSAO_APP_ID);
       this.statusBar.styleDefault();
       this.splashScreen.hide();
       timer(3000).subscribe(() => this.showSplash = false)
--- ionic3_back/src/servico/acessa-fcm-service.ts
+++ ionic3_back/src/servico/acessa-fcm-service.ts
@@ -4,6 +4,7 @@ import { VisitanteApi } from "../shared/sdk/services/custom/Visitante";
 import { Visitante } from "../shared/sdk/models/Visitante";
 import { DispositivoUsuarioApi } from "../shared/sdk/services/custom/DispositivoUsuario";
 import { DispositivoUsuario } from "../shared/sdk/models/DispositivoUsuario";
+import { Storage } from '@ionic/storage';
 
 @Injectable()
 export class AcessaFcmService {
@@ -19,60 +20,62 @@ export class AcessaFcmService {
     }
 
 
-    public executaValidacao(versaoAppId:number) {
-        this.storage.get("dispositivo").then((dado) => {
+    public executaValidacao(versaoAppId: number) {
+        this.storage.get("chave").then((dado) => {
             if (dado) {
-                this.registraVisita(dado);
+                console.log('Recuperou Chave');
+                this.registraVisitaApp(dado, versaoAppId);
             } else {
                 this.obtemTokenDispostivoUsuario(versaoAppId);
             }
         });
     }
-    public executaValidacaoFake(versaoAppId:number) {
-        this.storage.get("dispositivo").then((dado) => {
+    public executaValidacaoFake(versaoAppId: number) {
+        this.storage.get("chave").then((dado) => {
             if (dado) {
-                this.registraVisita(dado);
+                console.log('Recuperou Chave');
+                this.registraVisitaApp(dado, versaoAppId);
             } else {
                 this.obtemTokenDispostivoUsuarioFake(versaoAppId);
             }
         });
     }
 
-	
-    private registraMobile(chave) {
+
+    private registraMobile(chave, versaoAppId) {
         this.storage.set("chave", chave).then((successData) => {
-            this.registraVisita(chave);
+            this.registraVisitaApp(chave, versaoAppId);
         })
     }
-    private registraVisita(chave) {
-
+    private registraVisitaApp(chave, versaoAppId) {
+        console.log('Passou em visita: ', chave);
     }
-    private obtemTokenDispostivoUsuarioFake(versaoAppId:number) {
+    private obtemTokenDispostivoUsuarioFake(versaoAppId: number) {
         var token = '112231213215415615151515'
         console.log('Token fake: ', token);
-        let dispositivoUsuario : DispositivoUsuario = new DispositivoUsuario();
+        let dispositivoUsuario: DispositivoUsuario = new DispositivoUsuario();
         dispositivoUsuario.tokenFcm = token;
         dispositivoUsuario.versaoAppId = versaoAppId;
         this.dispositivoUsuarioSrv.CriaComUsuario(dispositivoUsuario)
             .subscribe((resultado: any) => {
-                console.log('Resultado:', resultado);
-                this.registraMobile(resultado);
+                console.log('Chave-Server:', resultado);
+                this.registraMobile(resultado, versaoAppId);
             })
     }
-    private obtemTokenDispostivoUsuario(versaoAppId:number) {
+    private obtemTokenDispostivoUsuario(versaoAppId: number) {
         this.fcm.subscribeToTopic('all');
         //alert('inscreveu');
-        let dispositivoUsuario : DispositivoUsuario = new DispositivoUsuario();
+        let dispositivoUsuario: DispositivoUsuario = new DispositivoUsuario();
         this.fcm.getToken().then(token => {
             //alert(token);
             dispositivoUsuario.tokenFcm = token;
             dispositivoUsuario.versaoAppId = versaoAppId;
             //alert(JSON.stringify(disppositivoUsuario));
             this.dispositivoUsuarioSrv.CriaComUsuario(dispositivoUsuario)
-            .subscribe((resultado: any) => {
-                console.log('Resultado:', resultado);
-                this.registraMobile(resultado);
-            })
+                .subscribe((resultado: any) => {
+                    console.log('Chave-Server:', resultado);
+                    this.registraMobile(resultado, versaoAppId);
+                })
         });
         this.fcm.onNotification().subscribe(data => {
             //alert('Recebeu notificacao')
@@ -96,7 +99,7 @@ export class AcessaFcmService {
     }
 
 
-    
+
     public obtemToken(visitanteCorrente: Visitante) {
         var token = '112231213215415615151515'
         console.log('Token fake: ', token);
@@ -110,43 +113,6 @@ export class AcessaFcmService {
 
 
 
-    public obtemToken1(visitanteCorrente: Visitante) {
-        this.fcm.subscribeToTopic('all');
-        alert('inscreveu');
-        this.fcm.getToken().then(token => {
-            alert(token);
-            //localStorage.setItem('token', token);
-            visitanteCorrente.fcmToken = token;
-            visitanteCorrente.dataHoraNotificacao = new Date();
-            alert(JSON.stringify(visitanteCorrente));
-            this.visitanteSrv.atualizaItem(visitanteCorrente.id, visitanteCorrente)
-                .subscribe(
-                    (resultado: any) => {
-                        alert('Sucesso:' + JSON.stringify(resultado))
-                    },
-                    (erro: any) => {
-                        alert('Erro:' + JSON.stringify(erro))
-                    });
-        });
-        this.fcm.onNotification().subscribe(data => {
-            alert('Recebeu notificacao')
-            if (data.wasTapped) {
-                alert('background');
-            } else {
-                alert('foreground');
-            }
-            let visitaNotificacao = new Visitante();
-            visitaNotificacao.versaoAppId = 789;
-            this.visitanteSrv.criaItem(visitaNotificacao)
-                .subscribe((resultado: any) => {
-                    alert('notificacao: ' + JSON.stringify(resultado));
-                })
 
-        });
-        this.fcm.onTokenRefresh().subscribe(token => {
-            //alert('token')
-        });
-
-    }
 
 }
