6864362888b107415e3ca7347a75d9d9ec953734
Paulo Alexandre <paulofore@gmail.com>
2019-10-24 22:25:27 -0200

    alterei versao_app

--- ionic3_back/src/app/const.ts
+++ ionic3_back/src/app/const.ts
@@ -3,7 +3,7 @@ export const MSG_ERRO_LOGIN = 'Email ou senha incorreto';
 export const MSG_CADASTRO_TAMANHO_SENHA = 'Mínimo de 8 caracteres.';
 export const MSG_CADASTRO_EMAIL = 'Digite seu email'
 
-export const VERSAO_APP_ID = 6;
+export const VERSAO_APP_ID = 7;
 
 // ListaOportunidade --> VERSAO_APP_ID = 6
 // ListaOportunidadePreco --> VERSAO_APP_ID = 7
