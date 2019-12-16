2e144f7c5a3a129ba19c293dda0e2306b2222278
Paulo Alexandre <paulofore@gmail.com>
2019-12-16 20:40:31 -0300

    lab

---
+++ ionic3_back_lab_credito/platforms/android/app/src/main/assets/www/build/10.js
@@ -0,0 +1,42 @@
+webpackJsonp([10],{
+
+/***/ 886:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "InicioFluxoPageModule", function() { return InicioFluxoPageModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(20);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__inicio_fluxo__ = __webpack_require__(532);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+var InicioFluxoPageModule = /** @class */ (function () {
+    function InicioFluxoPageModule() {
+    }
+    InicioFluxoPageModule = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["NgModule"])({
+            declarations: [
+                __WEBPACK_IMPORTED_MODULE_2__inicio_fluxo__["a" /* InicioFluxoPage */],
+            ],
+            imports: [
+                __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* IonicPageModule */].forChild(__WEBPACK_IMPORTED_MODULE_2__inicio_fluxo__["a" /* InicioFluxoPage */]),
+            ],
+        })
+    ], InicioFluxoPageModule);
+    return InicioFluxoPageModule;
+}());
+
+//# sourceMappingURL=inicio-fluxo.module.js.map
+
+/***/ })
+
+});
+//# sourceMappingURL=10.js.map
